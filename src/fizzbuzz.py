4371fec4f6b3f191314ec9aaf5ce13f185b6fe8b
Florent <florent.machen@gmail.com>
2023-05-17 14:19:14 +0200

    run unittest in pytest workflows

---
+++ src/fizzbuzz.py
@@ -0,0 +1,20 @@
+import argparse
+
+
+def fizzbuzz(n):
+    if n % 3 and n % 5 == 0:
+        return 'fizzbuzz'
+    elif n % 3 == 0:
+        return 'fizz'
+    elif n % 5 == 0:
+        return 'buzz'
+    else:
+        return str(n)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                        help='an integer for the fizzbuzz game')
+    args = parser.parse_args()
+    for i in range(args.integers[0]):
+        print(fizzbuzz(i + 1))
---
+++ tests/test_fizzbuzz.py
@@ -0,0 +1,36 @@
+import unittest
+
+from src.fizzbuzz import fizzbuzz
+
+
+class FizzBuzzTestCase(unittest.TestCase):
+
+    def test_python_test_setup_works(self):
+        self.assertTrue(True)
+
+    def test_it_returns_1_when_number_is_1(self):
+        # Arrange / Given
+
+        # Act / When
+        result = fizzbuzz(1)
+
+        # Assert / Then
+        self.assertEqual('1', result)
+
+    def test_it_returns_2_when_number_is_2(self):
+        # Arrange / Given
+
+        # Act / When
+        result = fizzbuzz(2)
+
+        # Assert / Then
+        self.assertEqual('2', result)
+
+    def test_it_returns_fizz_when_number_is_3(self):
+        # Arrange / Given
+
+        # Act / When
+        result = fizzbuzz(3)
+
+        # Assert / Then
+        self.assertEqual('fizz', result)
